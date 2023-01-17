a9afea449f17bc40df0b4b5babf7f5407ef51b11
moo_miz <moo_miz@naver.com>
2023-01-17 17:44:25 +0900

    feat: react-pdf install

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,44 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import Footer from "./Presentational/common/Footer";
 
 import Navigation from "./Presentational/common/Navigation";
+import InterviewPage from "./Presentational/pages/Interview/InterviewPage";
 import MainPage from "./Presentational/pages/MainPage";
 import ReviewPage from "./Presentational/pages/ReviewPage";
 import RoomPage from "./Presentational/pages/RoomPage";
 
 const expenses = [
   {
-    id: 'e1',
-    title: '면접 들어와',
+    id: "e1",
+    title: "면접 들어와",
     people: 4,
     date: new Date(2023, 1, 15),
-    content:'이 정보는 테스트용 정보 입니다.가나다라마바사아자차카타파하.이 정보는 테스트용 정보 입니다.가나다라마바사아자차카타파하'
-
-  }
-]
+    content:
+      "이 정보는 테스트용 정보 입니다.가나다라마바사아자차카타파하.이 정보는 테스트용 정보 입니다.가나다라마바사아자차카타파하",
+  },
+];
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
-    <BrowserRouter>
-      <AppContainer>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/review" element={<ReviewPage />} />
-          <Route path="/room/*" element={<RoomPage items={expenses}/>} />
-        </Routes>
-        <Footer />
-      </AppContainer>
-    </BrowserRouter>
+    <AppContainer>
+      {(pathname.includes("interview")) ? null:<Navigation /> }
+
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/review" element={<ReviewPage />} />
+        <Route path="/room/*" element={<RoomPage items={expenses} />} />
+        <Route path="/interview/*" element={<InterviewPage />} />
+      </Routes>
+
+      {(pathname.includes("interview")) ? null:<Footer /> }
+
+    </AppContainer>
   );
 }
 
 export default App;
 
-const AppContainer = styled.div`
-  position: relative;
-  min-height: 100%;
-  padding-bottom: 960px;
-`;
-
-// footer 고정할려고 padding-bottom을 줬는데, 간격이 너무 넒어져서 
-// 고칠 필요가 있음...
+const AppContainer = styled.div``;
