5325ea2537cdaa24b38d1bc89e68ee4a68ddb61f
김지현 <devhyun08@gmail.com>
2023-01-19 17:54:32 +0900

    feat: feat: feat(FE): mainpage/mainpage카테고리

--- frontend/src/Presentational/component/RoomListItem.jsx
+++ frontend/src/Presentational/component/RoomListItem.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import styled from "styled-components";
 
+import PasswordModal from './PasswordModal'
+
 function RoomListItem(props) {
+    //비밀번호 모달(시도까지만)
+    const [passwordModal, setPasswordModal] = useState(false);
+    function passwordHandler() {
+        if(passwordModal===false){
+            setPasswordModal(true); 
+        }
+        else{
+            setPasswordModal(false); 
+        }
+    }
+
     return(
         <RoomItem>
             <div className="rommtitle">
-                <h4>{props.title}</h4>
+                <h3>{props.title}</h3>
                 <p>{props.Participant}/{props.personnel}</p>
             </div>
             <p>{props.date}</p>
+            {/* <button onClick={passwordHandler}>비밀번호</button> */}
+            {passwordModal ? <PasswordModal />:null}
         </RoomItem>
     )
 }
@@ -22,8 +38,8 @@ const RoomItem = styled.li`
   border: 1px solid black;
   border-radius: 15px;
   width: 32%;
-  height: 100px;
-  padding: 2%;
+  height: 140px;
+  padding: 2% 3%;
   margin-bottom:2%;
   div{
     display: flex;
--- frontend/src/Presentational/layout/mainpage/MainBottom.jsx
+++ frontend/src/Presentational/layout/mainpage/MainBottom.jsx
@@ -1,14 +1,18 @@
+import { useState , useEffect } from "react";
 import styled from "styled-components";
 import RoomList from "../../component/RoomList";
-import React,{useState,useEffect,useCallback,useMemo} from "react";
+import TotalCategory from "../../component/TotalCategory"
+import MyCategory from "../../component/MyCategory"
+
 
 const DUMMY_DATA = [
   {
     id: 1,
     Participant: 2,
     personnel: 4,
-    title: "방제목",
+    title: "비밀방1",
     date: "23.01.24",
+    secret: true
   },
   {
     id: 2,
@@ -16,6 +20,7 @@ const DUMMY_DATA = [
     personnel: 4,
     title: "방제목",
     date: "23.01.24",
+    secret: false
   },
   {
     id: 3,
@@ -23,13 +28,15 @@ const DUMMY_DATA = [
     personnel: 4,
     title: "방제목",
     date: "23.01.24",
+    secret: false
   },
   {
     id: 4,
     Participant: 2,
     personnel: 4,
-    title: "방제목",
+    title: "비밀방2",
     date: "23.01.24",
+    secret: true
   },
   {
     id: 5,
@@ -37,6 +44,7 @@ const DUMMY_DATA = [
     personnel: 4,
     title: "방제목",
     date: "23.01.24",
+    secret: false
   },
   {
     id: 6,
@@ -44,13 +52,15 @@ const DUMMY_DATA = [
     personnel: 4,
     title: "방제목",
     date: "23.01.24",
+    secret: false
   },
   {
     id: 7,
     Participant: 2,
     personnel: 4,
-    title: "방제목",
+    title: "비밀방3",
     date: "23.01.24",
+    secret: true
   },
   {
     id: 8,
@@ -58,6 +68,7 @@ const DUMMY_DATA = [
     personnel: 4,
     title: "방제목",
     date: "23.01.24",
+    secret: false
   },
   {
     id: 9,
@@ -65,6 +76,7 @@ const DUMMY_DATA = [
     personnel: 4,
     title: "방제목",
     date: "23.01.24",
+    secret: false
   },
   {
     id: 10,
@@ -72,38 +84,105 @@ const DUMMY_DATA = [
     personnel: 4,
     title: "방제목",
     date: "23.01.24",
+    secret: false
   },
 ];
 
+const MY_ROOMS = [  {
+    id: 1,
+    Participant: 2,
+    personnel: 4,
+    title: "알밥",
+    date: "23.01.24",
+  },
+  {
+    id: 2,
+    Participant: 2,
+    personnel: 4,
+    title: "짬뽕",
+    date: "23.01.24",
+  },
+  {
+    id: 3,
+    Participant: 2,
+    personnel: 4,
+    title: "탕수육",
+    date: "23.01.24",
+  },
+]
+
 function MainBottom() {
-  const handleChange = (content) => {
-    console.log(content.target.value);
-  };
+    //로그인여부
+    const [isLogined,setIsLogined] = useState(false) //false:비로그인, true:로그인
+    //로그인(테스트용)
+    function loginBtn() {
+        if(isLogined===false){
+            setIsLogined(true)
+            console.log(isLogined)
+            const btnElement = document.getElementById('btn');
+            btnElement.innerText = '로그인상태';
+        }
+        else{
+            setIsLogined(false)
+            console.log(isLogined)
+            const btnElement = document.getElementById('btn');
+            btnElement.innerText = '지금은 비로그인';
+        }      
+    }
+    
+    //더미데이터(변경을 위해, useState에 넣어둠)
+    const [data, setData] = useState(DUMMY_DATA);
+    
+
+    //////////////////  <<<  total/my 사용자정렬  >>>>  ////////////////
+    //사용자 정렬
+    const [roomPosition,setRoomPosition] = useState(false) // false : total, true ; my
+    //버튼이 따른 변경
+    function roomSwitch(){
+        if(roomPosition){
+            setRoomPosition(false)
+            setData(DUMMY_DATA)
+        }
+        else{
+            setRoomPosition(true)
+            setData(MY_ROOMS)
+        }
+    }
+
+    useEffect(()=>{
+        if(isLogined===false){
+            setRoomPosition(false)
+            setData(DUMMY_DATA)
+        }
+        else{
+            setRoomPosition(true)
+            setData(MY_ROOMS)
+
+        }
+    },[isLogined])
+
 
   return (
     <Layout>
+         {/* 로그인 상태에 따른거(테스트용)  */}
+        <button id='btn' onClick={loginBtn}>지금은 비로그인</button>
+       
       <Header>
         <h1> LOOM LIST</h1>
         <Titlesection>
           <p>대충 설명이 들어가겠죠?</p>
           <div>
-            <button>TOTAL</button>
-            <button>MY</button>
+            <button onClick={roomSwitch}>TOTAL</button>
+            <button onClick={roomSwitch}>MY</button>
           </div>
         </Titlesection>
       </Header>
       <section>
         <Main>
-            <SearchWrapper>
-                <input type="text" className="search-input" onChange={handleChange} placeholder="검색어를 입력해주세용"/>
-            </SearchWrapper>
-          <div>
-            <button>모든 방</button>
-            <button>입장가능</button>
-          </div>
-          <LommListdiv>
-            <RoomList rooms={DUMMY_DATA} />
-          </LommListdiv>
+            {roomPosition ? <MyCategory/> : <TotalCategory/>}
+          <RoomListdiv>
+            <RoomList rooms={data} />
+          </RoomListdiv>
         </Main>
         <Footer>
           <button>방만들기</button>
@@ -136,11 +215,11 @@ const Main = styled.div`
   }
 `;
 
-const LommListdiv = styled.div``;
+const RoomListdiv = styled.div``;
 
 const Footer = styled.div`
   display: flex;
   flex-direction: row-reverse;
 `;
 
-const SearchWrapper = styled.div``;
+
