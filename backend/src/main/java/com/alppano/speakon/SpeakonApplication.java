9a780dce09499891a0a36d60cf52cf43b483d205
이희수 <64580070+hamchu@users.noreply.github.com>
2023-01-20 23:55:02 +0900

    chore: 백엔드 프로젝트 생성

---
+++ backend/src/main/java/com/alppano/speakon/SpeakonApplication.java
@@ -0,0 +1,13 @@
+package com.alppano.speakon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakonApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alppano/speakon/SpeakonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alppano.speakon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
