50978157fde4b8a2b1d80426a77a40112c714eeb
이희수 <64580070+hamchu@users.noreply.github.com>
2023-01-24 23:33:01 +0900

    fix: 면접방 생성 후 응답데이터에 현재인원 추가

--- backend/src/main/java/com/alppano/speakon/domain/interview_room/dto/InterviewRoomInfo.java
+++ backend/src/main/java/com/alppano/speakon/domain/interview_room/dto/InterviewRoomInfo.java
@@ -13,8 +13,7 @@ public class InterviewRoomInfo {
 
     private Long id;
     private String title;
-    // TODO: InterviewJoin 작업할 때 추가예정
-//    private int currentPersonCount;
+    private int currentPersonCount;
     private int maxPersonCount;
     private boolean secretRoom;
     private boolean finished;
@@ -23,6 +22,7 @@ public class InterviewRoomInfo {
     public InterviewRoomInfo(InterviewRoom dto) {
         this.id = dto.getId();
         this.title = dto.getTitle();
+        this.currentPersonCount = dto.getInterviewJoins().size();
         this.maxPersonCount = dto.getMaxPersonCount();
         this.secretRoom = dto.getPassword() == null ? false : true;
         this.finished = dto.getFinished() == 0 ? false : true;
