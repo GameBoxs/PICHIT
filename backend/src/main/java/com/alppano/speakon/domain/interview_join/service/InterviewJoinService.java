b01517b97e548e995c93e3c7cb13dcb606f419f6
이희수 <64580070+hamchu@users.noreply.github.com>
2023-01-24 23:55:35 +0900

    fix: 방의 인원이 남은 경우에만 면접참여 가능하도록 수정

--- backend/src/main/java/com/alppano/speakon/domain/interview_join/service/InterviewJoinService.java
+++ backend/src/main/java/com/alppano/speakon/domain/interview_join/service/InterviewJoinService.java
@@ -27,6 +27,11 @@ public class InterviewJoinService {
 
     @Transactional
     public InterviewJoinInfo createInterviewJoin(InterviewJoinRequest dto, Long userId) {
+        // 이미 참여 중인 면접방인 경우
+        if (interviewJoinRepository.findByUserIdAndInterviewRoomId(userId, dto.getInterviewRoomId()).isPresent()) {
+            throw new ResourceAlreadyExistsException("해당 면접방에 이미 참여중 입니다.");
+        }
+
         User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("회원"));
 
         InterviewRoom interviewRoom = interviewRoomRepository.findById(dto.getInterviewRoomId())
@@ -37,17 +42,18 @@ public class InterviewJoinService {
             throw new ResourceForbiddenException("비밀번호가 틀렸습니다.");
         }
 
+        // 참여인원과 최대인원이 같은 경우
+        if (interviewRoom.getInterviewJoins().size() == interviewRoom.getMaxPersonCount()) {
+            throw new ResourceForbiddenException("해당 방의 인원이 꽉 찼습니다.");
+        }
+
         InterviewJoin interviewJoin = InterviewJoin.builder()
                 .interviewRoom(interviewRoom)
                 .user(user)
                 .finished(0)
                 .build();
 
-        try {
-            interviewJoinRepository.saveAndFlush(interviewJoin);
-        } catch (DataIntegrityViolationException e) {
-            throw new ResourceAlreadyExistsException("해당 면접방에 이미 참여중 입니다.");
-        }
+        interviewJoinRepository.save(interviewJoin);
 
         return new InterviewJoinInfo(interviewJoin);
     }
@@ -58,7 +64,7 @@ public class InterviewJoinService {
                 .orElseThrow(() -> new ResourceNotFoundException("참여 중인 면접방이 아닙니다."));
 
         // 참여 취소하려는 면접참여 정보가 본인이 참여 중인 정보인지 확인
-        if(interviewJoin.getUser().getId() == userId) {
+        if (interviewJoin.getUser().getId() == userId) {
             interviewJoinRepository.delete(interviewJoin);
         } else {
             throw new ResourceForbiddenException("본인이 참여 중인 면접방만 참여 취소를 할 수 있습니다.");
