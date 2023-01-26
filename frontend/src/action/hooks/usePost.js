fefe6aea17ab7bfc069d0fb44c920633ef599b40
unknown <director20844@gmail.com>
2023-01-26 15:03:03 +0900

    feat: isLoading

--- frontend/src/action/hooks/usePost.js
+++ frontend/src/action/hooks/usePost.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 function usePost(target, body) {
   const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   //로그인 이후 리덕스 저장소에서 토큰 들고오기
   const token =
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzcGVha29uIiwibmFtZSI6IuydtO2drOyImCIsImlkIjo0NiwiZXhwIjoxNjc2NTA3ODMwLCJpYXQiOjE2NzQ2OTM0MzAsInVzZXJJZCI6Imtha2FvXzI2Mjk4Mzk0NjIifQ.R7JuJDcrX13tpKqqHxq_MDcNOzASPZUYPnLWlevKzmePM6InZPe3YEy0XkTD-HqRADzkpB2p9UYFcVYnwQwzBA";
@@ -13,10 +14,13 @@ function usePost(target, body) {
       .then((res) => {
         setData(res.data);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+      .finally(()=>{
+        setIsLoading(false)
+      })
   }, [target]);
 
-  return [data];
+  return {data, isLoading};
 }
 
 export default usePost;
