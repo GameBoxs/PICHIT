8214f2a13605f909cf60caebdb9ef994330c3c07
이희수 <2heedol@naver.com>
2023-01-31 09:34:45 +0900

    feat: 로그인 회원 정보 조회

--- backend/src/main/java/com/alppano/speakon/domain/user/service/UserService.java
+++ backend/src/main/java/com/alppano/speakon/domain/user/service/UserService.java
@@ -5,6 +5,7 @@ import com.alppano.speakon.domain.user.dto.UserInfoDto;
 import com.alppano.speakon.domain.user.entity.User;
 import com.alppano.speakon.domain.user.repository.UserRepository;
 import com.alppano.speakon.common.exception.ResourceNotFoundException;
+import com.alppano.speakon.security.LoginUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,4 +40,8 @@ public class UserService {
         return true;
     }
 
+    public UserInfoDto getMyUserInfo(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("존재하지 않는 회원입니다."));
+        return new UserInfoDto(user);
+    }
 }
