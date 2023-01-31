64b8f380d06f9f468ad6e9e7b0034183287821bf
이희수 <2heedol@naver.com>
2023-01-31 17:56:31 +0900

    feat: 면접 녹음 타임스탬프 등록

---
+++ backend/src/main/java/com/alppano/speakon/domain/recording_timestamp/repository/RecordingTimestampRepository.java
@@ -0,0 +1,10 @@
+package com.alppano.speakon.domain.recording_timestamp.repository;
+
+import com.alppano.speakon.domain.recording_timestamp.entity.RecordingTimestamp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RecordingTimestampRepository extends JpaRepository<RecordingTimestamp, Long> {
+    Optional<RecordingTimestamp> findByQuestionId(Long questionId);
+}
