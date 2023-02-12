78fe01791cc58382d55aebdca3d76b214538b3ce
moo_miz <moo_miz@naver.com>
2023-02-13 04:03:13 +0900

    fix(FE): 스터디 시작하기 오류 수정

--- frontend/src/Presentational/component/TotalCategory.jsx
+++ frontend/src/Presentational/component/TotalCategory.jsx
@@ -1,61 +1,81 @@
 import styled from "styled-components";
-import { useState} from "react";
-import {MdCancel} from 'react-icons/md';
+import { useState } from "react";
+import { MdCancel } from "react-icons/md";
+import { BiSearch } from "react-icons/bi";
 
 function TotalCategory(props) {
-    //검색
-    const [search, setSearch]=useState("");
-    function onChangeSearch(e){
-      e.preventDefault();
-      setSearch(e.target.value);
-    }
-    function onSearch(e){
-      e.preventDefault();
-      props.searchHandler(search)
-    }
-    function clearSearch(){
-      setSearch("")
-      props.searchHandler("")
-    }
+  //검색
+  const [search, setSearch] = useState("");
+  function onChangeSearch(e) {
+    e.preventDefault();
+    setSearch(e.target.value);
+  }
+  function onSearch(e) {
+    e.preventDefault();
+    props.searchHandler(search);
+  }
+  function clearSearch() {
+    setSearch("");
+    props.searchHandler("");
+  }
 
-    //sort선택
-    function sortSelect(e){
-      props.sortHandler(e)
-    }
+  //sort선택
+  function sortSelect(e) {
+    props.sortHandler(e);
+  }
 
   return (
     <div>
       <SearchWrapper>
-        <SearchForm onSubmit={e=>onSearch(e)}>
+        <SearchForm onSubmit={(e) => onSearch(e)}>
           <InputBox>
+            <div class="search__icon">
+              <BiSearch />
+            </div>
             <input
               type="text"
               value={search}
               onChange={onChangeSearch}
-              placeholder="검색어를 입력해주세용"
+              placeholder="검색어를 입력해주세요"
             />
             <RemoveBtn>
-              {search ? <MdCancel color="gray" size={23} onClick={clearSearch}/>:null}
+              {search ? (
+                <MdCancel color="gray" size={23} onClick={clearSearch} />
+              ) : null}
             </RemoveBtn>
           </InputBox>
-          <SubmitBtm type="submit">검색</SubmitBtm>
         </SearchForm>
-        
       </SearchWrapper>
       <SortCartegory>
-        <SortBtn onClick={()=>{sortSelect('')}}>등록 순</SortBtn>
-        <SortBtn onClick={()=>{sortSelect('title')}}>가나다 순</SortBtn>
-        <SortBtn onClick={()=>{sortSelect('startDate')}}>날짜 순</SortBtn>
+        <SortBtn
+          onClick={() => {
+            sortSelect("");
+          }}
+        >
+          등록 순
+        </SortBtn>
+        <SortBtn
+          onClick={() => {
+            sortSelect("title");
+          }}
+        >
+          가나다 순
+        </SortBtn>
+        <SortBtn
+          onClick={() => {
+            sortSelect("startDate");
+          }}
+        >
+          날짜 순
+        </SortBtn>
       </SortCartegory>
-      
     </div>
   );
 }
 
 export default TotalCategory;
 
-const SearchWrapper = styled.div`
-  `;
+const SearchWrapper = styled.div``;
 
 const SearchForm = styled.form`
   width: 100%;
@@ -64,37 +84,61 @@ const SearchForm = styled.form`
   align-items: center;
   margin: 1.5rem;
   padding-right: 3rem;
-  `
-  const InputBox = styled.div`
-    display: flex;
-    width: 100%;
-    background: #e8e8e8;
-    /* background-color: green; */
-    align-items: center;
-    border-radius: 1.625rem;
-    input{
-    width: 93%;
-    height: 2.5rem;
-    background: #e8e8e8;
-    outline: none;
-    border: none;
-    border-radius: 1.625rem;
-    padding: 0rem 3.5rem 0rem 1.5rem;
-    font-size: 1rem;
-  }
-  `
-  const RemoveBtn = styled.div`
-   display: flex;
-   margin: auto;
-    
-  `
-  const SubmitBtm = styled.button`
-    margin: 0% 1rem;
-  `
-  const SortCartegory = styled.div`
-    margin: 15px 0px;
-  `;
+`;
+const InputBox = styled.div`
+  display: flex;
+  width: 100%;
+  position: relative;
+  align-items: center;
+  border-radius: 1.625rem;
+  grid-row: 2 / 3;
+  grid-column: 3 / 4;
 
-  const SortBtn = styled.button`
-    
-  `
+  input {
+    display: flex;
+    width: 95%;
+    height: 4rem;
+    border: none;
+    border-radius: 1rem;
+    font-size: 1.4rem;
+    padding-left: 3.8rem;
+    box-shadow: inset 0.2rem 0.2rem 0.5rem var(--greyLight-2),
+      inset -0.2rem -0.2rem 0.5rem var(--white);
+    background: none;
+    font-family: inherit;
+    color: var(--greyDark);
+    &:focus {
+    outline: none;
+    box-shadow: 
+      0.3rem 0.3rem 0.6rem var(--greyLight-2),
+      -0.2rem -0.2rem 0.5rem var(--white);
+
+    + .search__icon {
+      color: var(--primary);
+    }
+  }
+
+  }
+  &::placeholder {
+    color: var(--greyLight-3);
+  }
+  
+  .search__icon {
+    height: 1rem;
+    position: absolute;
+    font-size: 2rem;
+    padding: 0 1rem;
+    display: flex;
+    color: var(--greyDark);
+    transition: 0.3s ease;
+  }
+`;
+const RemoveBtn = styled.div`
+  display: flex;
+  margin: auto;
+`;
+const SortCartegory = styled.div`
+  margin: 15px 0px;
+`;
+
+const SortBtn = styled.button``;
