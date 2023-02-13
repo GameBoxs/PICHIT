9303ebe6c8f576d5cb0841a99d6df1c9db72296a
moo_miz <moo_miz@naver.com>
2023-02-13 17:46:58 +0900

    feat(FE):방 생성하기 버튼 비로그인시 모달 막기

--- frontend/src/Presentational/layout/mainpage/BoardBodyBottom.jsx
+++ frontend/src/Presentational/layout/mainpage/BoardBodyBottom.jsx
@@ -2,12 +2,29 @@ import React, {useState} from "react";
 import styled from "styled-components";
 import CreateRoom from "../../component/CreateRoom";
 import Button from "../../common/Button";
+import { useSelector } from "react-redux";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
 
 function BoardBodyBottom() {
     //방생성하기
+    const token =useSelector((state)=>state.token)
     const [modalOpen, setModalOpen] = useState(false);
     const showModal = () => {
-      setModalOpen(true);
+      if (token){
+        setModalOpen(true);
+      }
+      else{
+        MySwal.fire({
+          title:"로그인이 필요한 서비스 입니다.",
+          text: "상단 메뉴에서 카카오 로그인을 이용해 주세요 ",
+          showConfirmButton: false,
+          icon: "warning",
+          timer: 1500,
+        });
+      }
     };
 
     return <Footer>
