ddc7c1e5b7de37ff78dd9318eca52a1802b8de10
김지훈 <ji_hun23@naver.com>
2023-02-15 17:06:56 +0900

    style(FE): Screen 주석 추가, 콘솔 로그 제거

--- frontend/src/Presentational/layout/Interview/Screen.jsx
+++ frontend/src/Presentational/layout/Interview/Screen.jsx
@@ -1,16 +1,21 @@
+/* ETC Import */
 import React from "react"
 import styled from "styled-components"
+
+/* Component Import */
 import DefaultScreen from "../../component/DefaultScreen"
 
 const Screen = ({number, info}) => {
-    // 최대 4명 중 빈 참여자 수
+    /* 최대 4명 중 참여하지 않은 빈 자리 수 */
     let blankNumber = 3-number;
 
+    /* 나를 제외한 참여자 화면 추가 */
     const allScreen = Array(number).fill(0).map((_, idx) => {
+        /* name - index에 해당하는 참여자 이름 */
         let name = JSON.parse(info.subscribers[idx].stream.connection.data).clientData;
         return <DefaultScreen key={idx} streamManager={info.subscribers[idx]} name={name} session={info.mySessionId} isNone={false}/>
     })
-    // 참여 하지 않은 사람 자리에 넣을 검은 화면
+    /* 빈 자리 수 만큼 검은 화면 추가 */
     const blankScreen = Array(blankNumber).fill(0).map((_, idx) => {
         return <DefaultScreen key={idx} isNone={true} />
     })
@@ -24,6 +29,7 @@ const Screen = ({number, info}) => {
 
 export default Screen
 
+/* Styled-Component */
 const FullScreen = styled.div`
     width: 51vw;
     height: 60vh;
