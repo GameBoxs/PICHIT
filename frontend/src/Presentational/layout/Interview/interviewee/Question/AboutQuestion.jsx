7799774ab468df6eb97df1c8f5cd1fa34161fd7e
unknown <director20844@gmail.com>
2023-02-16 21:45:21 +0900

    feat(FE): interviewee Tooltip 추가

--- frontend/src/Presentational/layout/Interview/interviewee/Question/AboutQuestion.jsx
+++ frontend/src/Presentational/layout/Interview/interviewee/Question/AboutQuestion.jsx
@@ -14,6 +14,17 @@ const AboutQuestion = ({ highlight, finishHandler }) => {
         <TipMark>
           <BsQuestionCircleFill />
         </TipMark>
+        <Tooltip>
+          우측에서 질문을 선택할 수 있습니다.
+          <br />
+          모든 팀원에게 선택된 질문이 뜨며,
+          <br />
+          질문 끝내기를 통해 질문을 종료하고
+          <br />
+          피드백을 마칠 수 있습니다.
+          <br />
+          끝난 질문은 다시 못하니 유의해주세요!{" "}
+        </Tooltip>
       </SubNav>
 
       {/* 질문 상세 내용 */}
@@ -29,6 +40,33 @@ const AboutQuestion = ({ highlight, finishHandler }) => {
 
 export default memo(AboutQuestion);
 
+const Tooltip = styled.div`
+  position: absolute;
+  right: -7.5rem;
+  top: 4rem;
+  padding: 1.5rem;
+  line-height: 1.7rem;
+  color: var(--white);
+  background-color: var(--textColor);
+  opacity: 0;
+  width: 20rem;
+  z-index: 20;
+  font-weight: lighter;
+  
+  &:before {
+		pointer-events: none;
+		position: absolute;
+		z-index: -1;
+		content: '';
+		border-style: solid;
+		left: 10rem;
+    top: -20px;
+		border-width: 0 10px 20px 10px;
+		border-color: transparent transparent var(--textColor) transparent;
+		transition-property: top;
+	}
+`;
+
 const TipMark = styled.div`
   color: var(--greyLight-2);
   font-size: 1.2em;
@@ -41,6 +79,10 @@ const TipMark = styled.div`
   &:hover {
     path {
       color: var(--greyDark);
+    }
+
+    ~ ${Tooltip} {
+      opacity: 0.8;
     }
   }
 `;
